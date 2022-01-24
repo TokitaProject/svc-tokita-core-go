788ebd876d067d78bfa53522c82babc4865f25b4
Elbo Shindi Pangestu <elbo@engineer.com>
2022-01-24 21:14:10 +0700

    tambah config error bind json

--- utils/config/config.go
+++ utils/config/config.go
@@ -1,7 +1,8 @@
 package config
 
 var Config = map[string]string{
-	"sql.not.found": "sql: no rows in result set",
+	"sql.not.found":   "sql: no rows in result set",
+	"error.bind.json": "parameter tidak tepat",
 }
 
 func Get(name string) string {
