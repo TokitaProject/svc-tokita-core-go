68724a80c31be090ea6974b10bc401079caf4b22
Elbo Shindi Pangestu <elbo.shindi@uii.ac.id>
2022-02-16 20:28:02 +0700

    add param

--- domain/boilerplate/repository.go
+++ domain/boilerplate/repository.go
@@ -9,21 +9,21 @@ type MysqlRepository interface {
 	Exec(...database.QueryConfig) error
 	GenerateUUID() (uint64, error)
 
-	GetAll(map[string]interface{}) ([]valueobject.Boilerplate, error)
-	GetOne(map[string]interface{}) (valueobject.Boilerplate, error)
+	GetAll(param map[string]interface{}) ([]valueobject.Boilerplate, error)
+	GetOne(param map[string]interface{}) (valueobject.Boilerplate, error)
 
-	Store([]string, []interface{}) database.QueryConfig
-	Update(map[string]interface{}, map[string]interface{}) database.QueryConfig
-	Delete(map[string]interface{}) database.QueryConfig
+	Store(column []string, data []interface{}) database.QueryConfig
+	Update(param map[string]interface{}, data map[string]interface{}) database.QueryConfig
+	Delete(param map[string]interface{}) database.QueryConfig
 }
 
 type OracleRepository interface {
 	Exec(...database.QueryConfig) error
 
-	GetAll(map[string]interface{}) (response []valueobject.Boilerplate, err error)
-	GetOne(map[string]interface{}) (response valueobject.Boilerplate, err error)
+	GetAll(param map[string]interface{}) (response []valueobject.Boilerplate, err error)
+	GetOne(param map[string]interface{}) (response valueobject.Boilerplate, err error)
 
-	Store([]string, []interface{}) database.QueryConfig
-	Update(map[string]interface{}, map[string]interface{}) database.QueryConfig
-	Delete(map[string]interface{}) database.QueryConfig
+	Store(column []string, data []interface{}) database.QueryConfig
+	Update(param map[string]interface{}, data map[string]interface{}) database.QueryConfig
+	Delete(param map[string]interface{}) database.QueryConfig
 }
