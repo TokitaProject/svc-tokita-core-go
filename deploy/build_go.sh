4c5ff981cae5aaa3ac7be88f55c079a85c826ab4
Elbo Shindi Pangestu <elbo.shindi@uii.ac.id>
2022-04-11 11:31:52 +0700

    update deployment

--- deploy/build_go.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#Compile golang files to binary executable
-env GOOS=linux CGO_ENABLED=0 go build -a -ldflags="-s -w" -installsuffix cgo -o main .
--- run_example.sh
+++ run_example.sh
@@ -13,7 +13,10 @@ export ORACLE_DB_PORT="1521"
 export ORACLE_DB_SERVICE_NAME="transdb1"
 
 export PORT="80"
-
+export ENV="local"
 export GIN_MODE=debug
+
+# Bearer digunakan untuk melakukan request public api. Variabel env ini digunakan hanya untuk keperluan local development. Tidak ada dokumentasi, silakan bertanya.
+export BEARER="bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdmMtbG9naW4tdWlpIiwic3ViIjoiZWxiby5zaGluZGktUEFva1dvS2lYbTY5UnBrZTVOWG04VGxLa3h2ckcxSDYiLCJpYXQiOjE2NDQyMTk5NTYsImV4cCI6MTY0NTQyOTU1Nn0.reAb-WHHimHD-CMd9otYVC4iIMFaJJ1ex-SGASYNzQQ"
 
 nodemon --exec go run main.go --signal SIGTERM
