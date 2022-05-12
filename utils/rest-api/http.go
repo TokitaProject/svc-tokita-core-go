f138041e601f911b91ad6c39090167aa7b8977cc
Elbo Shindi Pangestu <elbo.shindi@uii.ac.id>
2022-05-12 10:03:50 +0700

    add put and post

--- utils/rest-api/http.go
+++ utils/rest-api/http.go
@@ -1,6 +1,7 @@
 package restapi
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -27,6 +28,96 @@ func HttpGet(param map[string]interface{}) (result map[string]interface{}, err e
 	}
 
 	request.URL.RawQuery = query.Encode()
+
+	response, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		return
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return
+	}
+
+	if response.StatusCode > 300 {
+		err = fmt.Errorf(string(body))
+		return
+	}
+
+	err = json.Unmarshal(body, &result)
+
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func HttpPost(param map[string]interface{}) (result map[string]interface{}, err error) {
+	url := param["url"].(string)
+	data := param["data"].([]byte)
+
+	request, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(data))
+
+	if err != nil {
+		return
+	}
+
+	// FOR LOCAL ONLY
+	if os.Getenv("ENV") == "local" {
+		request.Header.Set("Authorization", os.Getenv("BEARER"))
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		return
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return
+	}
+
+	if response.StatusCode > 300 {
+		err = fmt.Errorf(string(body))
+		return
+	}
+
+	err = json.Unmarshal(body, &result)
+
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func HttpPut(param map[string]interface{}) (result map[string]interface{}, err error) {
+	url := param["url"].(string)
+	data := param["data"].([]byte)
+
+	request, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(data))
+
+	if err != nil {
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	// FOR LOCAL ONLY
+	if os.Getenv("ENV") == "local" {
+		request.Header.Set("Authorization", os.Getenv("BEARER"))
+	}
 
 	response, err := http.DefaultClient.Do(request)
 
