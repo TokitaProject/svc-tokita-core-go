d10fc42433d8b78f5e00a31979da7a77a49aa836
Elbo Shindi Pangestu <elbo.shindi@uii.ac.id>
2022-06-17 15:00:48 +0700

    add MergeUnique

--- utils/maps/maps.go
+++ utils/maps/maps.go
@@ -9,3 +9,17 @@ func Merge(maps ...map[string]interface{}) map[string]interface{} {
 	}
 	return result
 }
+
+func MergeUnique(maps ...map[string]interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	mapUnique := make(map[string]bool)
+	for _, m := range maps {
+		for k, v := range m {
+			if _, ok := mapUnique[k]; !ok {
+				mapUnique[k] = true
+				result[k] = v
+			}
+		}
+	}
+	return result
+}
