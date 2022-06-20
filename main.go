b862b94dd899a0b5577f8702bdbc487d997f3eb1
Elbo Shindi Pangestu <elbo.shindi@uii.ac.id>
2022-06-20 21:20:05 +0700

    add connect db func

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 	_boilerplateHttpDeliver "svc-boilerplate-golang/domain/boilerplate/delivery/http"
@@ -14,18 +15,32 @@ import (
 func main() {
 	routers := gin.Default()
 
-	mysql, err := database.SetupMysqlDatabaseConnection()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	// oracle, err := database.SetupOracleDatabaseConnection()
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
+	mysql := ConnectMySQL()
+	// oracle := ConnectOracle()
 
 	boilerplateMysqlRepository := _boilerplateRepository.NewMysqlBoilerplateRepository(mysql)
 	boilerplateUsecase := _boilerplateUsecase.NewBoilerplateUsecase(boilerplateMysqlRepository)
 	_boilerplateHttpDeliver.NewBoilerplateHttpHandler(boilerplateUsecase, routers)
 
 	routers.Run(":" + os.Getenv("PORT"))
+}
+
+func ConnectMySQL() (mysql *sql.DB) {
+	mysql, err := database.SetupMysqlDatabaseConnection()
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return
+}
+
+func ConnectOracle() (oracle *sql.DB) {
+	oracle, err := database.SetupOracleDatabaseConnection()
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return
 }
