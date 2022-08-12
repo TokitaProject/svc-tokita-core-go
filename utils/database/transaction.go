2e0d41303b8e604f74e00136d357ec6cc8113828
Elbo Shindi Pangestu <elbo.shindi@uii.ac.id>
2022-08-12 09:55:33 +0700

    optimize

--- utils/database/transaction.go
+++ utils/database/transaction.go
@@ -17,28 +17,26 @@ func New(tech string, table string, command string) QueryConfig {
 
 func ExecTransaction(db *sql.DB, query ...QueryConfig) (err error) {
 	tx, err := db.Begin()
-
 	if err != nil {
 		return
 	}
-
+	listStatement := make(map[string]*sql.Stmt)
 	for _, builder := range query {
-		statement, err := tx.Prepare(builder.Result.Query)
-
-		defer tx.Rollback()
-
+		if _, found := listStatement[builder.Result.Query]; !found {
+			listStatement[builder.Result.Query], err = tx.Prepare(builder.Result.Query)
+			if err != nil {
+				return fmt.Errorf("query: %s | value: %s | message: %s", builder.Result.Query, builder.Result.Value, err.Error())
+			}
+			defer tx.Rollback()
+			defer listStatement[builder.Result.Query].Close()
+		}
+		_, err = listStatement[builder.Result.Query].Exec(builder.Result.Value...)
 		if err != nil {
 			return fmt.Errorf("query: %s | value: %s | message: %s", builder.Result.Query, builder.Result.Value, err.Error())
 		}
-
-		defer statement.Close()
-
-		_, err = statement.Exec(builder.Result.Value...)
-
-		if err != nil {
-			return fmt.Errorf("query: %s | value: %s | message: %s", builder.Result.Query, builder.Result.Value, err.Error())
-		}
+		// For debuging logical error
+		// log.Println(builder.Result.Query, builder.Result.Value)
 	}
-
-	return tx.Commit()
+	tx.Commit()
+	return
 }
